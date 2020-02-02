3e13165704cfaf5e3889e9975f54d1665c21f3b9
Daniel Correa <dcorrea2207@gmail.com>
2020-02-02 17:10:23 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,9 +1,14 @@
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+
+def display_board(the_game_board)
+  puts " #{the_game_board[0]} | #{the_game_board[1]} | #{the_game_board[2]} "
   puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts " #{the_game_board[3]} | #{the_game_board[4]} | #{the_game_board[5]} "
   puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts " #{the_game_board[6]} | #{the_game_board[7]} | #{the_game_board[8]} "
 end
 
 # code your input_to_index and move method here!
+
+def move(board,index, token)
+   board[index] = token
+end
